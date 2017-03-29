3532ee5d99940081d9d30bfd7f0b8afa9e02b0c0
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-03-29 19:56:04 +0200

    [Improve] Better position

--- v2/js/main.js
+++ v2/js/main.js
@@ -5,16 +5,13 @@ let mvMatrix = mat4.create();
 let pMatrix = mat4.create();
 
 let squares = [];
-let _NB_SQUARES = 100;
-let deltaZ = 0.4;
+let deltaZ = 0.1;
 //buffers
 let squareVertexPositionBuffer = null;
-let squareVertexColorBuffer = null;
 let texture = null;
 
 let isAnimated = true;
 let renderingModes = null;
-let backgroundColor = 0.0;
 
 function initGL(canvas) {
     try {
@@ -80,10 +77,10 @@ function initShaders() {
 
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
     shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
     gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
 
+    shaderProgram.alphaUniform = gl.getUniformLocation(shaderProgram, "uAlpha");
     shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
@@ -125,6 +122,8 @@ function renderSquares() {
         gl.bindBuffer(gl.ARRAY_BUFFER, square.textureCoordBuffer);
         gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, square.textureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
+        gl.uniform1f(shaderProgram.alphaUniform, square.alpha);
+
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.uniform1i(shaderProgram.samplerUniform, 0);
@@ -137,7 +136,7 @@ function renderSquares() {
 }
 
 function animateSquares() {
-    for (let i = 0; i < _NB_SQUARES; i++) {
+    for (let i = 0; i < nbSquares; i++) {
         squares[i].animate(deltaZ);
     }
 }
@@ -145,12 +144,10 @@ function animateSquares() {
 function initSquares() {
     //Let's define only 1 buffer th at will be used to render several squares.
     //That way we'll use less memory
-    let buffers = Square.createBuffers();
-    squareVertexPositionBuffer = buffers[0];
-    squareVertexColorBuffer = buffers[1];
+    squareVertexPositionBuffer = Square.createBuffers();
 
-    for (let i = 0; i < _NB_SQUARES; i++) {
-        squares.push(new Square(deltaZ));
+    for (let i = 0; i < nbSquares; i++) {
+        squares.push(new Square(i, deltaZ));
     }
 }
 
@@ -160,19 +157,16 @@ function initSquares() {
 function render() {
     requestAnimFrame(render);
 
-    gl.clearColor(backgroundColor, backgroundColor, backgroundColor, 1.0);
-
+    //gl.clearColor(1, 1, 1, 1);
+    //gl.colorMask(false, false, false, true);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     mat4.perspective(pMatrix, 45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
 
+
     renderSquares();
 
     if (isAnimated) {
-
-        if (backgroundColor < 1.0)
-            backgroundColor += 0.005;
-
         animateSquares();
     }
 }
@@ -233,6 +227,11 @@ function main() {
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+        //gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+        gl.enable(gl.BLEND);
+        gl.disable(gl.DEPTH_TEST);
 
         render();
     }
--- v2/js/square.class.js
+++ v2/js/square.class.js
@@ -2,42 +2,30 @@
  * Created by gwennael.buchet on 28/03/17.
  */
 const squareWidth = 1.6;
+const screenWidth = 16;
+const screenHeight = 12;
+const halfScreenWidth = screenWidth / 2.0;
+const halfScreenHeight = screenHeight / 2.0;
+const nbColumns = Math.floor((screenWidth / squareWidth));
+const nbRows = Math.floor((screenHeight / squareWidth));
+const nbSquares = nbColumns * nbRows;
 
 class Square {
 
-    constructor(deltaZ) {
-        //compute new tex coords
-        this.textureCoords = [
-            0.0, 0.0,
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0
-        ];
+    constructor(index, deltaZ) {
 
+        //compute position of this swuare on the "wall of square", which is a matrix
+        let row = Math.floor(index / nbColumns);
+        let column = index - (row * nbColumns);
+
+        this.x = -halfScreenWidth + (column * squareWidth + 0.2);
+        this.y = -halfScreenHeight + (row * squareWidth + 0.2);
+        this.z = -8 + (Math.random() * deltaZ);
+
+        //compute tex coords
         this.textureCoordBuffer = gl.createBuffer();
         this.textureCoordBuffer.itemSize = 2;
         this.textureCoordBuffer.numItems = 4;
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.textureCoordBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.textureCoords), gl.STATIC_DRAW);
-
-        this.invalidate(deltaZ);
-    }
-
-    invalidate(deltaZ) {
-        //set a random new position
-        this.x = -5 + Math.random() * 10;
-        this.y = -5 + (Math.random() * 10);
-        this.z = -8 + (Math.random() * deltaZ);
-
-        //new animation speed
-        this.speed = 5 + Math.random() * 8;
-        this.time = 0.0;
-
-        let screenWidth = 14;
-        let screenHeight = 10;
-        let halfScreenWidth = screenWidth / 2;
-        let halfScreenHeight = screenHeight / 2;
-
         //compute the new texture coordinates, depending on the position on the screen
         let texWidth = squareWidth / screenWidth;
         let texHeight = squareWidth / screenHeight;
@@ -56,12 +44,25 @@ class Square {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.textureCoordBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.textureCoords), gl.STATIC_DRAW);
+
+        this.invalidate(deltaZ);
+    }
+
+    invalidate(deltaZ) {
+
+        this.z = -8 + (Math.random() * deltaZ);
+
+        //new animation speed
+        this.speed = 0.001 + Math.random() / 100;
+        this.alpha = 0.0;
+        this.tmpAlpha = 0.0;
     }
 
     animate(deltaZ) {
-        this.time += this.speed;
+        this.tmpAlpha += this.speed;
+        this.alpha = Math.sin(this.tmpAlpha);
 
-        if (this.time > 100) {
+        if (this.tmpAlpha > 360) {
             this.invalidate(deltaZ);
         }
     }
@@ -80,20 +81,7 @@ class Square {
         positionBuffer.itemSize = 3;
         positionBuffer.numItems = 4;
 
-
-        let colorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-        let colors = [
-            1, 1, 1, 1,
-            1, 1, 1, 1,
-            1, 1, 1, 1,
-            1, 1, 1, 1
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-        colorBuffer.itemSize = 4;
-        colorBuffer.numItems = 4;
-
-        return [positionBuffer, colorBuffer];
+        return positionBuffer;
     }
 
 }
