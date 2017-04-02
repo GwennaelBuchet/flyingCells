f0b3d0babb1316eeb80923206e87dfbaa4a2fd1d
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-04-02 20:16:44 +0200

    [Improve] remove ghost code

--- v2/js/utils.js
+++ v2/js/utils.js
@@ -40,11 +40,11 @@ function toggleFullscreen(element, glContext, normalDimension) {
         isFullScreen = true;
     }
     else {
-        if(document.exitFullscreen) {
+        if (document.exitFullscreen) {
             document.exitFullscreen();
-        } else if(document.mozCancelFullScreen) {
+        } else if (document.mozCancelFullScreen) {
             document.mozCancelFullScreen();
-        } else if(document.webkitExitFullscreen) {
+        } else if (document.webkitExitFullscreen) {
             document.webkitExitFullscreen();
         }
 
@@ -53,34 +53,4 @@ function toggleFullscreen(element, glContext, normalDimension) {
 
         isFullScreen = false;
     }
-
-
-    /*var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
-     (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
-     (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
-     (document.msFullscreenElement && document.msFullscreenElement !== null);
-
-     var docElm = document.documentElement;
-     if (!isInFullScreen) {
-     if (docElm.requestFullscreen) {
-     docElm.requestFullscreen();
-     } else if (docElm.mozRequestFullScreen) {
-     docElm.mozRequestFullScreen();
-     } else if (docElm.webkitRequestFullScreen) {
-     docElm.webkitRequestFullScreen();
-     } else if (docElm.msRequestFullscreen) {
-     docElm.msRequestFullscreen();
-     }
-     } else {
-     if (document.exitFullscreen) {
-     document.exitFullscreen();
-     } else if (document.webkitExitFullscreen) {
-     document.webkitExitFullscreen();
-     } else if (document.mozCancelFullScreen) {
-     document.mozCancelFullScreen();
-     } else if (document.msExitFullscreen) {
-     document.msExitFullscreen();
-     }
-     }
-     */
 }
